71009d1eab89ea0d82323553b3a3384374d44fb2
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2022-08-17 22:09:57 +0100

    Factory contract deploys Certificate

---
+++ contracts/CertaineFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./CertaineERC721.sol";
+import "./CertaineMember.sol";
+
+contract CertaineFactory {
+
+    CertaineERC721[] public certificates;
+
+    function addCertificate(
+        string memory _certificateName,
+        string memory _symbol,
+        string memory _tokenUri
+    ) public onlyCertaine {
+        CertaineERC721 certificate = new CertaineERC721(
+            _certificateName,
+            _symbol
+        );
+        certificate.createCertificate(tokenUri);
+    }
+}
